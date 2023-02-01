013ac16b4bc020bda3c39eb8265b9544540f930b
João Pedro <jpbritoalm@gmail.com>
2023-02-01 10:38:30 -0300

    BicicletasGaleria-js

--- js/script.js
+++ js/script.js
@@ -41,3 +41,21 @@ function eventosPerguntas(pergunta){
 }
 
 perguntas.forEach(eventosPerguntas)
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarimagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches
+    if (media){
+        galeriaContainer.prepend(img)
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click',trocarimagem)
+}
+galeria.forEach(eventosGaleria);
