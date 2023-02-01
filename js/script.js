38049b162a78f4482ba67cf1644e43c4dc98060c
João Pedro <jpbritoalm@gmail.com>
2023-02-01 10:28:30 -0300

    Perguntas-js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ function ativarProduto(parametro){
 }
 
 parametros.forEach(ativarProduto)
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const respota = document.getElementById(controls)
+
+    respota.classList.toggle('ativa')
+    pergunta.setAttribute('aria-expanded',respota.classList.contains('ativa'))
+}
+
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
